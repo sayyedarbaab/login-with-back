745bade09084a9b6103f5940b6880a01cecd393c
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-30 19:17:56 +0530

    add commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Validation code for input
+
+var email=document.forms['form']['email'];
+var email=document.forms['form']['password'];
+
+var email_error=document.getElementById('email_error')
+var pass_error=document.getElementById('email_error')
